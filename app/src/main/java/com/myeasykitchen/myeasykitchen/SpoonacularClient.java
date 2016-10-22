48ed91fa0b66dbd42558c1e8aaef8bb50cdb6f9b
Ali Tejani <Ali Tejani>
2016-10-21 20:57:13 -0500

    spoonacular client

---
+++ app/src/main/java/com/myeasykitchen/myeasykitchen/SpoonacularClient.java
@@ -0,0 +1,19 @@
+package com.myeasykitchen.myeasykitchen;
+
+/**
+ * Created by Ali on 10/21/2016.
+ */
+
+public class SpoonacularClient {
+    public SpoonacularClient() {
+
+    }
+
+    public String getCost(Item item) {
+        return "";
+    }
+
+    public String getNutritionalInformation(Item item) {
+        return "";
+    }
+}
